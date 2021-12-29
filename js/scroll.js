1b941527d4a2cc9dfb717ac8dcb33fbdc1b45a66
Felipev98 <felipe_v_spd@hotmail.com>
2021-12-28 23:30:50 -0400

    add: tecnologies section

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,4 @@
-window.sr = ScrollReveal();
-sr.reveal('.nav-item',{
-duration: 3000,
-origin:'bottom',
-distance:'-50px'
 
-})
 window.sr = ScrollReveal();
 sr.reveal('#banner-imagen',{
 duration: 3000,
@@ -41,6 +35,49 @@ origin:'bottom',
 distance:'-50px'
 
 })
+
+window.sr = ScrollReveal();
+sr.reveal('.info-programacion',{
+duration: 3000,
+origin:'right',
+distance:'-150px'
+})
+window.sr = ScrollReveal();
+sr.reveal('.info-programacion2',{
+duration: 3000,
+origin:'right',
+distance:'-150px'
+})
+window.sr = ScrollReveal();
+sr.reveal('.imagen-felipe',{
+duration: 3000,
+origin:'right',
+distance:'-150px'
+})
+window.sr = ScrollReveal();
+sr.reveal('#card-aboutme',{
+duration: 3000,
+origin:'right',
+distance:'-150px'
+})
+window.sr = ScrollReveal();
+sr.reveal('.card-bodys',{
+duration: 3000,
+origin:'right',
+distance:'-150px'
+})
+window.sr = ScrollReveal();
+sr.reveal('.tabs',{
+duration: 3000,
+origin:'bottom',
+distance:'-50px'
+
+})
+
+
+
+
+
 
 
 
